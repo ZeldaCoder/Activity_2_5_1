8a9ced0d7c4d5965a2125ac43f4ea864c7ac43a5
ZeldaCoder <jedediah.c.muthalaly@gmail.com>
2024-10-28 15:27:18 -0700

    More stuff added

--- Player.java
+++ Player.java
@@ -3,15 +3,32 @@
  * 
  * A Player class the PhraseSolverGame
  */
-import java.util.Scanner;
 
 public class Player
 {
   /* your code here - attributes */
+  private String name;
+  private int points;
 
-  /* your code here - constructor(s) */ 
+  /* your code here - constructor(s) */
+  Player(String inputName) {
+    name = inputName;
+    points = 0;
+
+    System.out.print("Welcome " + name);
+  } 
 
   /* your code here - accessor(s) */ 
+  int getPoints() {
+    return points;
+  }
+
+  String getName() {
+    return name;
+  }
 
   /* your code here - mutator(s) */ 
+  void setPoints(int newPoints) {
+    points = newPoints;
+  }
 }
